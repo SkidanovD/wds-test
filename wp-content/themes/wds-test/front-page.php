97b2bf98d76eab5a8e860274c20443dfbb6fb282
SkidanovD <skidanchix@gmail.com>
2024-08-17 01:30:40 +0300

    Added block «Special offer»

--- wp-content/themes/wds-test/front-page.php
+++ wp-content/themes/wds-test/front-page.php
@@ -22,6 +22,20 @@ if (!empty($featured_product['category'])) {
 	];
 	$featured_product_slider = wc_get_products($args);
 }
+$special_offer = get_field('special_offer');
+$args_fp = [
+	'status' => 'publish',
+	'limit'  => 3,
+	'order' => 'ASC',
+	'terms'    => 'featured',
+];
+$featured_products = wc_get_products($args_fp);
+$args_np = [
+	'status' => 'publish',
+	'limit'  => 3,
+	'order' => 'DESC',
+];
+$new_products = wc_get_products($args_np);
 
 get_header();
 ?>
@@ -84,7 +98,7 @@ get_header();
 									<div class="swiper-wrapper">
 										<?php foreach ($featured_product_slider as $product) : ?>
 											<div class="swiper-slide">
-												<a class="min-product">
+												<a href="<?php echo $product->get_permalink(); ?>" class="min-product">
 													<div class="image-wrapper">
 														<?php do_action( 'woocommerce_before_shop_loop_item_title' ); ?>
 													</div>
@@ -107,6 +121,83 @@ get_header();
 					</div>
 				</div>
 			<?php endif; ?>
+			<?php if (!empty($special_offer)) : ?>
+				<div class="special-offer-block">
+					<div class="special-offer-wrapper width-container dsflex">
+						<div class="left-column">
+							<div class="banner dsflex-ai-center">
+								<?php if (!empty($special_offer['title'])) : ?>
+									<div class="banner-decor"><?php echo $special_offer['title']; ?></div>
+								<?php endif; ?>
+								<div class="image-column">
+									<?php if (!empty($special_offer['image'])) : ?>
+										<div class="image-wrapper">
+											<img class="image-block-contain" src="<?php echo $special_offer['image']['url']; ?>" alt="<?php echo $special_offer['image']['alt']; ?>" loading="lazy">
+										</div>
+									<?php endif; ?>
+								</div>
+								<div class="text-wrapper">
+									<?php if (!empty($special_offer['subtitle'])) : ?>
+										<p class="subtitle h5"><?php echo $special_offer['subtitle']; ?></p>
+									<?php endif; ?>
+									<?php if (!empty($special_offer['title'])) : ?>
+										<h2 class="title h1"><?php echo $special_offer['title']; ?></h2>
+									<?php endif; ?>
+									<?php if (!empty($special_offer['text'])) : ?>
+										<div class="text h4"><?php echo $special_offer['text']; ?></div>
+									<?php endif; ?>
+									<?php if (!empty($special_offer['button'])) : ?>
+										<div class="button-wrapper">
+											<?php if (!empty($special_offer['button'])) : ?>
+												<a href="<?php echo $special_offer['button']['url']; ?>" class="button button-default hover-default" title="<?php echo $special_offer['button']['title']; ?>"<?php echo !empty($special_offer['button']['target']) ? 'target="'.$special_offer['button']['target'].'"' : ''; ?>><?php echo $special_offer['button']['title']; ?></a>
+											<?php endif; ?>
+										</div>
+									<?php endif; ?>
+								</div>
+							</div>
+						</div>
+						<div class="center-column">
+							<?php if (!empty($special_offer['featured_title'])) : ?>
+								<p class="special-list-title h4"><?php echo $special_offer['featured_title']; ?></p>
+							<?php endif; ?>
+							<?php if (!empty($featured_products)) : ?>
+								<div class="products-list">
+									<?php foreach ($featured_products as $product) : ?>
+										<a href="<?php echo $product->get_permalink(); ?>" class="special-product dsflex">
+											<div class="image-column"><?php do_action( 'woocommerce_before_shop_loop_item_title' ); ?></div>
+											<div class="text-column">
+												<p class="special-product-title"><?php echo $product->get_title(); ?></p>
+												<div class="special-product-price"><?php echo $product->get_price_html(); ?></div>
+											</div>
+										</a>
+									<?php endforeach; ?>
+								</div>
+							<?php endif; ?>
+						</div>
+						<div class="right-column">
+							<?php if (!empty($special_offer['new_title'])) : ?>
+								<p class="special-list-title h4"><?php echo $special_offer['new_title']; ?></p>
+							<?php endif; ?>
+							<?php if (!empty($new_products)) : ?>
+								<div class="products-list">
+									<?php foreach ($new_products as $product) : ?>
+										<a href="<?php echo $product->get_permalink(); ?>" class="special-product dsflex-ai-items">
+											<div class="image-column"><?php do_action( 'woocommerce_before_shop_loop_item_title' ); ?></div>
+											<div class="text-column">
+												<p class="special-product-title"><?php echo $product->get_title(); ?></p>
+												<div class="special-product-price"><?php echo $product->get_price_html(); ?></div>
+											</div>
+										</a>
+									<?php endforeach; ?>
+								</div>
+							<?php endif; ?>
+						</div>
+					</div>
+				</div>
+			<?php endif; ?>
+			
+
+
       </div>
 	</main><!-- #main -->
 
